51e04348e5584c8cf6bb162f169caa4a80fd91f7
Zern <75311133+BG47510@users.noreply.github.com>
2023-09-30 20:12:23 +0200

    Update lequipe.py

--- direct/lequipe/lequipe.py
+++ direct/lequipe/lequipe.py
@@ -2,11 +2,22 @@
 # par github.com/BG47510
 import requests
 
-def pompe():
-	url = f"https://www.dailymotion.com/player/metadata/video/x2lefik"
-	response = requests.get(url).json()
-	flux = response["qualities"]["auto"][0]["url"]
-	m3u8 = requests.get(flux).text
-	print(m3u8)
 
-#pompe()
+# idvideo (str): L'ID de la vidéo Dailymotion.
+def dailymotion(idvideo: str):
+    # Récupère les flux Dailymotion en fonction de l'ID vidéo.
+    try:
+        url = f"https://www.dailymotion.com/player/metadata/video/{idvideo}"
+        response = requests.get(url).json()
+        flux = response["qualities"]["auto"][0]["url"]
+        m3u8 = requests.get(flux).text
+        print(m3u8)
+    # Exception: si une erreur se produit lors de la récupération des flux.
+    except Exception as m3u8:
+        m3u8 = "https://raw.githubusercontent.com/BG47510/Zap/main/assets/error.m3u8"
+        print(m3u8)
+
+
+if __name__ == "__main__":
+    idvideo = "x2lefik"
+    dailymotion(idvideo)
