9c9ca6ed97cc5bae023afb1f335b61b85c4819a0
Zern <75311133+BG47510@users.noreply.github.com>
2024-01-18 19:53:40 +0100

    Update lequipe.sh

--- lequipe/lequipe.sh
+++ lequipe/lequipe.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 python3 lequipe/lequipe.py > lequipe/lequipe.m3u8
-python3 lequipe/lequipe.py > lequipe/lequipe1.m3u8
-python3 lequipe/lequipe.py > lequipe/lequipe2.m3u8
-python3 lequipe/lequipe.py > lequipe/lequipe3.m3u8
-python3 lequipe/lequipe.py > lequipe/lequipe4.m3u8
-python3 lequipe/lequipe.py > lequipe/lequipe5.m3u8
 
 exit 0
