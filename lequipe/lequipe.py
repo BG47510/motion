113f77975d10e95fb0b467eabb259aef0b2e6934
Zern <75311133+BG47510@users.noreply.github.com>
2024-01-22 15:15:40 +0100

    Update lequipe.py

--- lequipe/lequipe.py
+++ lequipe/lequipe.py
@@ -5,6 +5,7 @@ import requests
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0",
+    'Accept-Language': 'fr,fr-FR;q=0.8',
     "Referer": "https://www.lequipe.fr/tv/"
 }
 
