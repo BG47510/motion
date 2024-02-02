a8ddb12875dcd013c86d472c12e6c92d5629654c
Zern <75311133+BG47510@users.noreply.github.com>
2024-02-02 15:13:36 +0100

    Update motion.py

--- motion.py
+++ motion.py
@@ -11,17 +11,15 @@ headers = {
 erreur = requests.get("https://raw.githubusercontent.com/BG47510/Zap/main/assets/error.m3u8").text
 
 s = requests.session()
-s
+
 def snif(url):
     base = s.get(url, headers=headers)
     lien = (base).text
     flux = lien.replace("http://s2.callofliberty.fr/HLS-AES/", "")
-   # print(flux)
     if base.status_code == 200:
         print(flux)
     else:
         print(erreur)
-
 
 m3u8 = snif(sys.argv[1])
 print(m3u8)
